1d4617e038c8b4851484c4c9a23d4535cbbf0b7a
Ged <xiaoerlang0359@sina.cn>
2026-05-09 00:55:36 +0800

    finish 17

--- exercises/16_mysed/16_mysed.c
+++ exercises/16_mysed/16_mysed.c
@@ -10,14 +10,56 @@ int parse_replace_command(const char* cmd, char** old_str, char** new_str) {
     }
 
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    int len = strlen(cmd);
+    *old_str = (char*)malloc(len);
+    *new_str = (char*)malloc(len);
+    int i = 2;
+    int old_i = 0;
+    int new_i = 0;
+    while (cmd[i] && cmd[i]!='/'){
+        (*old_str)[old_i] = cmd[i];
+        i++;
+        old_i++;
+    }
+    (*old_str)[old_i] = '\0';
+    if (cmd[i] != '/'){
+        return -1;
+    }
+    i++;
+    while (cmd[i] && cmd[i]!='/'){
+        (*new_str)[new_i] = cmd[i];
+        i++;
+        new_i++;
+    }
+    (*new_str)[new_i] = '\0';
+    if (cmd[i] != '/'){
+        return -1;
+    }
 
     return 0;
 }
 
 void replace_first_occurrence(char* str, const char* old, const char* new) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char * pos = strstr(str, old);
+    if (pos == NULL){
+        printf("found none!!!");
+        return;
+    }
+    int str_len = strlen(str);
+    int old_len = strlen(old);
+    for (char* p = pos; p<=(pos + str_len-old_len); p++){
+        *p = *(p+old_len);
+    }
+    int new_len = strlen(new);
+    for (char* p = (str+str_len); p >= pos; p--){
+        *(p+new_len) = *(p);
+    }
+    int i=0;
+    for (char* p=pos; p<(pos+new_len);p++){
+        *p = new[i];
+        i++; 
+    }
 }
 
 int main(int argc, char* argv[]) {
